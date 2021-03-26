accd68c33adcdf58959cafe6aaea38f7d676ba51
gjgstyle <1095126893@qq.com>
2021-03-26 15:22:22 +0800

    eureku-client update

--- repoEureka/eureka-client/src/main/java/com/pratice/eurekaclient/controllor/UserController.java
+++ repoEureka/eureka-client/src/main/java/com/pratice/eurekaclient/controllor/UserController.java
@@ -2,6 +2,7 @@ package com.pratice.eurekaclient.controllor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +18,23 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	@PostMapping("/reigst")
-	public void reigst() {
-
+	@PostMapping("/regist")
+	public ResponseBodyEntity<Void> reigst(@RequestBody UserEntity user) {
+		ResponseBodyEntity<Void> result = new  ResponseBodyEntity<Void> ();
+		try {
+			userService.regist(user);
+			result.setState("200");
+			result.setMessage("注冊成功！");
+		}catch(Exception e) {
+			result.setState("500");
+			result.setMessage("注冊失敗！");
+		}
+		return result;
 	}
 
 	@PostMapping("/login")
 	public ResponseBodyEntity<Void> Login(@RequestParam("userName")String userName,@RequestParam("password")String password) {
 		UserEntity user = userService.getUserInfo(userName);
-
 
 		ResponseBodyEntity<Void> result = new  ResponseBodyEntity<Void> ();
 		if(user.getPassword().equals(password)) {
